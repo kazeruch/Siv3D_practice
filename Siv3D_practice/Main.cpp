5a42ef0e1b0ad69e311394d77c9b350cf38c5445
kazeruch <kazukirock0417@gmail.com>
2024-03-24 16:59:48 +0900

    Tutorial 7 done

--- Siv3D_practice/Main.cpp
+++ Siv3D_practice/Main.cpp
@@ -2,50 +2,29 @@
 
 void Main()
 {
-	/*bool a = true;
+	// 背景を白色にする
+	// Scene::SetBackground(Palette::White);
 
-	int32 b = 123;
+	// 背景を黒色にする
+	// Scene::SetBackground(Palette::Black);
 
-	double c = 0.5;
+	// 背景色を HTML カラーで指定する
+	// Scene::SetBackground(Palette::Aliceblue);
 
-	size_t d = 100;
+	// 背景色を RGB で指定する
+	// Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
+	// Scene::SetBackground(ColorF{ 0.8 });
 
-	Print << U"a: " << a;
-
-	Print << U"b: " << b;
-
-	Print << U"c: " << c;
-
-	Print << U"d: " << d;*/
-
-	//char32 a = U'A';
-
-	//String b = U"Hello";
-
-	//// FilePath は String の別名
-	//FilePath c = U"example/windmill.png";
-
-	//Print << U"a: " << a;
-
-	//Print << U"b: " << b;
-
-	//Print << U"c: " << c;
-
-	Array<int32> a = { 10, 20, 50, 100 };
-
-	Optional<double> b;
-
-	Print << U"a: " << a;
-
-	Print << U"b: " << b;
-
-	b = 12.3;
-
-	Print << U"b: " << b;
+	// 背景色を HSV で指定する
+	// Scene::SetBackground(HSV{ 220.0, 0./4, 1.0 });
+	// Scene::SetBackground(HSV{ 220.0 }); ( s = 1.0, v = 1.0 )
 
 	while (System::Update())
 	{
-		
+		const double hue = (Scene::Time() * 60);
+
+		// 背景色を HSV で指定する
+		Scene::SetBackground(HSV{ hue });
 	}
 }
 
